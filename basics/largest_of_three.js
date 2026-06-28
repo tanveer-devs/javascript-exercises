15fe2e4b5b7db9d8d1b7f4e2e030853f0d62046c
Tanveer Ahmed <tanveerchandio432@gmail.com>
2026-06-29 00:08:31 +0500

    Largest of three solved

---
+++ basics/largest_of_three.js
@@ -0,0 +1,10 @@
+const a = 15;
+const b = 20;
+const c = 30;
+if (a > b && a > c) {
+    console.log("A is largest!");
+}else if (b > a && b > c) {
+    console.log("B is largest!");
+}else if (c > a && c > b) {
+    console.log("B is largest!");
+}
