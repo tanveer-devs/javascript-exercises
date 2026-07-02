bd61a39633d9a6a6caf7405921f4d89a63af27ca
Tanveer Ahmed <tanveerchandio432@gmail.com>
2026-07-02 22:50:37 +0500

    ATM simulator created

---
+++ mini_projects/atm_sim.js
@@ -0,0 +1,11 @@
+const balance = 5000;
+while(true){
+    switch (key) {
+        case 1:
+            console.log("Check Balance");
+            break;
+    
+        default:
+            break;
+    }
+}
